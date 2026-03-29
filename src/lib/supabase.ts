06962d87d08b54916a1a019689de55d5ce10a6f4
Carson <carson@Arkovas-Mac-mini.local>
2026-03-29 19:16:37 -0400

    fix: use SUPABASE_SERVICE_ROLE_KEY env var

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseServiceKey = process.env.SUPABASE_SECRET_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
